a70f3cbc46d53318da55106edbb828e238677a73
Hardikag17 <54525688+Hardikag17@users.noreply.github.com>
2021-09-03 00:37:08 +0530

    resolved some bugs

--- src/pages/Gusser.jsx
+++ src/pages/Gusser.jsx
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router";
-import ImageButton from "../components/ImageButton";
-import ImageInput from "../components/ImageInput";
-import Clock from "../images/clock.svg";
-import "../css/gusser.css";
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router';
+import ImageButton from '../components/ImageButton';
+import ImageInput from '../components/ImageInput';
+import Clock from '../images/clock.svg';
+import '../css/gusser.css';
 
 const Gusser = ({ socket }) => {
-  const [guess, setGuess] = useState("");
-  const [hintList, setHintList] = useState(["Waiting for Clues..."]);
+  const [guess, setGuess] = useState('');
+  const [hintList, setHintList] = useState(['Waiting for Clues...']);
   const [score, setScore] = useState(0);
   const location = useLocation();
   const [roundfromBackend, setRoundFromBackend] = useState(1);
 
   useEffect(() => {
-    socket.emit("gusserid", location.state.gusserid);
+    socket.emit('gusserid', location.state.gusserid);
 
-    socket.on("round-change-from-backend", (round) => {
+    socket.on('round-change-from-backend', (round) => {
       setRoundFromBackend(round);
     });
 
-    socket.on("scores", (game) => {
-      console.log("game:", game);
+    socket.on('scores', (game) => {
+      console.log('game:', game);
       //document.getElementById('scoreTeamRed').innerHTML = game[0].TeamScore;
       if (roundfromBackend % 2 == 0) {
         setScore(game[0].TeamScore);
@@ -36,55 +36,56 @@ const Gusser = ({ socket }) => {
 
   //  On clickong Enter button
   const guessSubmitted = () => {
-    socket.emit("guessSubmission", guess);
+    socket.emit('guessSubmission', guess);
     console.log(guess);
-    alert("Guess Submited...");
+    alert('Guess Submited...');
   };
   useEffect(() => {
-    socket.on("game-ended", (gameValue) => {
+    socket.on('game-ended', (gameValue) => {
       if (gameValue == 1) {
-        window.location.href = "/";
+        localStorage.clear();
+        window.location.href = '/';
       }
     });
   }, [socket]);
 
   // final array from host
-  socket.on("gusserHints", (arr) => {
-    console.log("HINT LIST ", arr);
+  socket.on('gusserHints', (arr) => {
+    console.log('HINT LIST ', arr);
     setHintList(arr);
   });
 
   return (
-    <div className="gusser">
-      <div className="gusser__bg"></div>
-      <div className="gusser__teamranks d-flex justify-content-between px-3">
-        <h3 className="my-auto" style={{ color: "#ffffff" }}>
+    <div className='gusser'>
+      <div className='gusser__bg'></div>
+      <div className='gusser__teamranks d-flex justify-content-between px-3'>
+        <h3 className='my-auto' style={{ color: '#ffffff' }}>
           Team Points
         </h3>
-        <h3 className="my-auto" style={{ color: "#603913" }}>
+        <h3 className='my-auto' style={{ color: '#603913' }}>
           {score}
         </h3>
       </div>
-      <div className="gusser__hints">
+      <div className='gusser__hints'>
         {hintList.map((hint, index) => {
           return <div key={index}>{hint}</div>;
         })}
       </div>
-      <div className="gusser__timer d-flex align-items-baseline">
-        <img src={Clock} alt="time" />
+      <div className='gusser__timer d-flex align-items-baseline'>
+        <img src={Clock} alt='time' />
         <h3>0:30</h3>
       </div>
-      <div className="gusser__enterdiv">
-        <h3 className="fw-bold">Guess the Word</h3>
+      <div className='gusser__enterdiv'>
+        <h3 className='fw-bold'>Guess the Word</h3>
         <br />
         <ImageInput
-          text="Enter your Guess"
+          text='Enter your Guess'
           change={(e) => setGuess(e.target.value)}
         />
         <ImageButton
-          value="ENTER"
+          value='ENTER'
           clickMe={guessSubmitted}
-          classlist="mt-3 gusser__enterbtn"
+          classlist='mt-3 gusser__enterbtn'
         />
       </div>
     </div>
--- src/pages/TeamRed.jsx
+++ src/pages/TeamRed.jsx
@@ -1,45 +1,46 @@
 // Team Red Players will enter their hinst here
-import React, { useState, useEffect } from "react";
-import ImageButton from "../components/ImageButton";
-import ImageInput from "../components/ImageInput";
-import Clock from "../images/clock.svg";
-import "../css/teamred.css";
-const axios = require("axios");
+import React, { useState, useEffect } from 'react';
+import ImageButton from '../components/ImageButton';
+import ImageInput from '../components/ImageInput';
+import Clock from '../images/clock.svg';
+import '../css/teamred.css';
+const axios = require('axios');
 
 const TeamRed = ({ socket }) => {
-  const [hint, setHint] = useState("");
+  const [hint, setHint] = useState('');
   const [roundfromBackend, setRoundFromBackend] = useState(0);
   const [chatmsgSent, setchatmsgSent] = useState(0);
   const [score, setScore] = useState(0);
-  const [randomword, setRandomWord] = useState(" ... ");
+  const [randomword, setRandomWord] = useState(' ... ');
 
   // getting the random word
   useEffect(() => {
     axios({
-      method: "get",
-      url: "http://localhost:5000/randomword",
+      method: 'get',
+      url: 'http://localhost:5000/randomword',
     })
       .then((res) => {
-        console.log("axios ", res.data);
+        console.log('axios ', res.data);
         setRandomWord(res.data);
       })
       .catch((err) => console.error(err));
   }, []);
 
   useEffect(() => {
-    socket.on("game-ended", (gameValue) => {
+    socket.on('game-ended', (gameValue) => {
       if (gameValue == 1) {
-        window.location.href = "/";
+        localStorage.clear();
+        window.location.href = '/';
       }
     });
 
-    socket.on("round-change-from-backend", (round) => {
+    socket.on('round-change-from-backend', (round) => {
       setchatmsgSent(1);
       setRoundFromBackend(round);
     });
 
-    socket.on("scores", (game) => {
-      console.log("game:", game);
+    socket.on('scores', (game) => {
+      console.log('game:', game);
       setScore(game[0].TeamScore);
       //document.getElementById('scoreTeamBlue').innerHTML = game[1].TeamScore;
     });
@@ -48,56 +49,56 @@ const TeamRed = ({ socket }) => {
     //   document.getElementById('Timer').innerHTML = minutes + ':' + seconds;
     // });
 
-    socket.on("guessed-wrong", (wrong) => {
+    socket.on('guessed-wrong', (wrong) => {
       alert(`Guesser guessed wrong,Now ${2 - wrong} chances left`);
     });
   }, [socket]);
 
-  socket.emit("guessingTeam", roundfromBackend);
+  socket.emit('guessingTeam', roundfromBackend);
 
-  socket.on("random-word", (word) => {
-    console.log("randomWord", word);
-    document.querySelector(".red__randomword").innerHTML = `" ${word} "`;
+  socket.on('random-word', (word) => {
+    console.log('randomWord', word);
+    document.querySelector('.red__randomword').innerHTML = `" ${word} "`;
   });
 
   const sendHint = () => {
-    socket.emit("msgListMake", { hint, room: "Team Red" });
-    document.querySelector(".red__input").value = "";
+    socket.emit('msgListMake', { hint, room: 'Team Red' });
+    document.querySelector('.red__input').value = '';
   };
 
   return (
-    <div className="red__bg">
-      <div className="red__enterhint text-center">
+    <div className='red__bg'>
+      <div className='red__enterhint text-center'>
         <h3>
-          Enter a Word simmilar to{" "}
-          <span className="red__randomword" style={{ color: "red" }}>
+          Enter a Word simmilar to{' '}
+          <span className='red__randomword' style={{ color: 'red' }}>
             " {randomword} "
           </span>
         </h3>
         <br />
         <ImageInput
-          text="Type your word here..."
+          text='Type your word here...'
           change={(e) => setHint(e.target.value)}
-          classList="red__input"
+          classList='red__input'
         />
         <br />
         <ImageButton
-          value="ENTER"
-          classlist="red__enterbtn"
+          value='ENTER'
+          classlist='red__enterbtn'
           clickMe={sendHint}
         />
       </div>
-      <div className="red__timer d-flex align-items-baseline">
-        <img src={Clock} alt="time" />
+      <div className='red__timer d-flex align-items-baseline'>
+        <img src={Clock} alt='time' />
         <h3>
-          <span id="Timer"></span>
+          <span id='Timer'></span>
         </h3>
       </div>
-      <div className="red__teamranks d-flex justify-content-between px-3">
-        <h3 className="my-auto" style={{ color: "#ffffff" }}>
+      <div className='red__teamranks d-flex justify-content-between px-3'>
+        <h3 className='my-auto' style={{ color: '#ffffff' }}>
           Score: {score}
         </h3>
-        <h3 className="my-auto" style={{ color: "#603913" }}>
+        <h3 className='my-auto' style={{ color: '#603913' }}>
           Round: <span>{roundfromBackend - 1}</span>
         </h3>
       </div>
